abf71db08edb6bd34a18a39e74a9853f14490c00
cheniel <danielchen627@gmail.com>
2014-09-27 01:21:36 -0700

    merging

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,35 +15,57 @@
 //= require turbolinks
 //= require_tree .
 
-var APP_GLBLS = {
-
+var APP = {
+	categories: [
+		{
+			email: 'movie.lover@rt.com', 
+			category: 'movies'
+		},
+		{
+			email: 'sportsFan600@gmail.com', 
+			category: 'sports'
+		}
+	],
+	bodyText: "Bacon ipsum dolor sit amet short loin chicken pancetta, shankle cow salami brisket venison. Tongue chicken boudin meatloaf. Pork belly t-bone venison, corned beef short ribs tri-tip pork loin prosciutto bacon. Prosciutto cow porchetta ball tip, chuck leberkas frankfurter ground round salami shankle. \n\nBacon landjaeger meatball pancetta kevin pig venison leberkas beef ribs pork chop strip steak jerky tri-tip. Boudin corned beef filet mignon bresaola ground round bacon beef. Shankle beef drumstick strip steak, meatball shoulder tenderloin boudin kevin jowl turducken bacon venison. Shankle biltong cow pork loin, beef capicola beef ribs brisket pancetta salami. Jerky ham hock turkey cow pork chop, spare ribs t-bone landjaeger swine bacon short loin biltong kevin porchetta pastrami.\n\nTurducken salami shank shoulder pancetta spare ribs sausage jowl. Corned beef kevin t-bone ground round andouille sirloin. Pancetta capicola drumstick filet mignon prosciutto. Ham flank pork brisket kevin, tongue fatback sirloin beef leberkas hamburger short ribs.\n\nShoulder andouille pancetta, spare ribs landjaeger brisket meatball pork short ribs prosciutto strip steak filet mignon porchetta. Pork chop jerky pancetta short ribs jowl, hamburger pork pastrami landjaeger bresaola kevin beef. Tongue kevin meatloaf flank pork belly. Chicken porchetta sausage, prosciutto tenderloin filet mignon pancetta. Beef ribs biltong salami boudin, pig beef short ribs corned beef jowl ground round ham jerky sausage swine. Jerky tenderloin meatloaf corned beef chicken jowl.\n\nTri-tip hamburger drumstick porchetta beef ribs meatball ground round pork chop doner, pork belly salami. Shank turkey leberkas chicken venison pork loin ground round salami. Kielbasa flank ham hock, hamburger jerky ribeye leberkas pork loin venison jowl pork belly meatloaf brisket boudin tail. Short ribs pig frankfurter ribeye, pork belly drumstick ground round strip steak kevin tongue. Leberkas rump chuck frankfurter brisket beef ribs pork belly jowl shoulder flank meatloaf turducken pork. Kielbasa salami ham hock pork strip steak pork chop. Pancetta biltong tail pork belly short loin flank pork chop kevin sirloin fatback shoulder beef.\n\nDoes your lorem ipsum text long for something a little meatier? Give our generator a try… it’s tasty!",
+	resultWords: [],
+	wordIndex: 0,
 }
-
-var CATEGORY_OPTIONS = [
-	{
-		email: 'movie.lover@rt.com', 
-		category: 'movies'
-	},
-	{
-		email: 'sportsFan600@gmail.com', 
-		category: 'sports'
-	}
-]
 
 function main() {
 	setUpCategoryAutofill();
+	getResult();
+	$('#results').keypress(function(e) {
+		if ( APP.wordIndex < APP.resultWords.length ) {
+			$('#results').val( 
+				$('#results').val()
+				+ APP.resultWords[APP.wordIndex] 
+				+ ' ');
+			APP.wordIndex++;
+		}
+		return false; // surpress the user input from being added
+	});
 }
 
 function setUpCategoryAutofill() {
 	var categoryBox = $('#category-suggestion-container');
 	categoryBox.append('<ul id="category-suggestions">');
-	for (var key in CATEGORY_OPTIONS) {
-		categoryBox.append($('<li>' + CATEGORY_OPTIONS[key].email + '</li>'));
+	for (var key in APP.categories) {
+		categoryBox.append($('<li>' + APP.categories[key].email + '</li>'));
 	}	
 	categoryBox.append('</ul>');
 	categoryBox.hide();
 	$('#category').focus(function(){categoryBox.show()});
 	$('#category').blur(function() {categoryBox.hide()})
+}
+
+function getResult() {
+	
+	
+	// fill in ajax request here
+	var result = APP.bodyText
+
+	APP.wordIndex = 0;
+	APP.resultWords = result.split(' ');
 
 }
 
