54b44fc85a2da9f35b45ecf08c08308cd625ad16
Joseph Nguyen <vietjew@gmail.com>
2014-09-26 22:30:59 -0700

    Set up topics controller

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,2 +1,12 @@
 class TopicsController < ApplicationController
+  
+  def index
+    @topics = Topic.all
+    
+    respond_to do |format|
+      format.html 
+      format.json { render json: @topics }
+    end
+  end
+  
 end
