3c59b4ea4fcb01ceb44b8be6e461c23955d752ff
Joseph Nguyen <vietjew@gmail.com>
2014-09-27 01:00:45 -0700

    Fix topic controller

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,12 +1,36 @@
 class TopicsController < ApplicationController
   
   def index
-    @topics = Topic.all
-    
+    @topic = Topic.find_by_title(params[:topic])
     respond_to do |format|
-      format.html 
-      format.json { render json: @topics }
+      format.html
+      format.json { render json: @topic }
     end
+    
+    if params[:topic] == "Movies"
+      @rtapi = Api::RTApi.new("Mission Impossible")
+      @summary = @rtapi.get_summary()
+      fail
+    elsif topic == "Sports"
+      @summary = "Some sports shit"
+    end
+  end
+  
+  def show
+    @topic = Topic.find_by_title(params[:topic])
+    if params[:topic] == "Movies"
+      @rtapi = Api::RTApi.new(query)
+      @summary = rtapi.get_summary()
+      @summary
+    elsif topic == "Sports"
+      #code for sports api
+    end
+  end
+  
+  private
+  
+  def topics_params
+    params.require(:topic).permit(:query)
   end
   
 end
